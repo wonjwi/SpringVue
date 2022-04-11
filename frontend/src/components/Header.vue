49179e0cfdb23ce59423bc290ca5e2dcd3d02d3a
원지연 <yh3347@naver.com>
2022-04-11 16:24:32 +0900

    Add Header.vue and Logo Image

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-navbar class="header-div">
+    <b-navbar-brand>
+      <img class="logo-img" src="@/assets/raon_logo.gif" />
+    </b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+.header-div {
+  margin: 20px 0;
+}
+.logo-img {
+  width: 120px;
+  margin: 0 40px;
+}
+</style>
