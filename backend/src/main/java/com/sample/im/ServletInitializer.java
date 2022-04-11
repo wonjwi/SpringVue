87ee9aa104bb9b4441ee853fc12fba78eb5a9be5
원지연 <yh3347@naver.com>
2022-04-11 11:01:27 +0900

    Initial commit

---
+++ backend/src/main/java/com/sample/im/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sample.im;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BackendApplication.class);
+	}
+
+}
