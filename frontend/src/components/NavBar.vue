0ce324f41d91ee057632dcc86d998b942de0e2fa
원지연 <yh3347@naver.com>
2022-04-11 16:24:57 +0900

    Add Navbar.vue

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-button-group class="menu-div" vertical>
+    <MenuItem
+      v-for="(menuItem, index) in menuList"
+      class="menu-item"
+      :key="index"
+      :main="menuItem.main"
+      :sub="menuItem.sub"
+      @setUrl="setUrl"
+    />
+  </b-button-group>
+</template>
+
+<script>
+import MenuItem from "@/components/MenuItem.vue";
+
+export default {
+  name: "NavBar",
+  components: {
+    MenuItem,
+  },
+  props: {
+    menuList: Array,
+  },
+  methods: {
+    setUrl(url) {
+      this.$emit("setUrl", url);
+    },
+  },
+};
+</script>
+
+<style>
+.menu-div {
+  width: 100%;
+}
+.menu-item {
+  height: 50px;
+}
+</style>
