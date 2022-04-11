09af6e2cae621ecc41d7ab40c32a3e78a75abbc3
원지연 <yh3347@naver.com>
2022-04-11 16:25:31 +0900

    Add MenuItem and SubMenuItem.vue

---
+++ frontend/src/components/SubMenuItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-dropdown-item class="sub-menu" @click="clickMenu(url)">
+    {{ name }}
+  </b-dropdown-item>
+</template>
+
+<script>
+export default {
+  name: "SubMenuItem",
+  props: {
+    name: String,
+    url: String,
+  },
+  methods: {
+    clickMenu(url) {
+      this.$emit("setUrl", url);
+    },
+  },
+};
+</script>
+
+<style></style>
