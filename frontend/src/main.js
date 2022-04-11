0576dfe926f249d2f8f862ea6dfd0cdd01d6b65d
원지연 <yh3347@naver.com>
2022-04-11 16:19:40 +0900

    Install and Import BootstrapVue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+
+// Use BootstrapVue
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
